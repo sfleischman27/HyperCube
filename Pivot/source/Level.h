1ac8afb484b156cdc0ecd97331cee7417efe40e0
Sarah Fleischman <smf267@cornell.edu>
2023-02-20 19:23:50 -0500

    fleshed out Level model. still need to add mesh type once known

--- Pivot/source/Level.h
+++ Pivot/source/Level.h
@@ -5,6 +5,10 @@
 //  Light model that has getters and setters for the level mesh, starting position
 //  and plane, as well as any other level data we add.
 //
+//  storage for the level mesh once loaded
+//  includes level default data will be placed into gameModel then updated there
+//  this default data will be used to restart levels
+//
 //  Created by Sarah Fleischman on 2/19/23.
 //
 
@@ -12,12 +16,73 @@
 #define Level_h
 #include <cugl/cugl.h>
 
+using namespace cugl;
+
 /**
- *  Storage for the level mesh once loaded
- *  Includes level default data (starting location, starting plane)
- *  The default data will be placed in the Game Model and will be updated there
- *  The default data here will be used to restart levels
+ * A class representing a level and its starting data
  */
+class Level{
+  
+#pragma mark State
+private:
+    /** Player starting location */
+    Vec3 _startLoc;
+    /** Plane starting plane */
+    Vec3 _startNorm;
+    /** Level mesh object */
+    // TODO: figure out what type this should be (_mesh)
+    
+public:
+    /** A public accessible, read-only version of the starting location */
+    const Vec3& startLoc;
+    /** A public accessible, read-only version of the starting plane */
+    const Vec3& startNorm;
+}
+
+#pragma mark Main Functions
+public:
+    /**
+     * Creates the model state.
+     *
+     * @param startLoc          The starting location of the player
+     * @param startNorm        The starting norm of the plane
+     * @param mesh                   The mesh object
+     */
+    TileModel(Vec3 startLoc, Vec3 startNorm): startLoc(_startLoc), startNorm(_startNorm) {
+        // set attributes
+        setPlayerLoc(startLoc);
+        setPlaneNorm(startNorm);
+        setMesh();
+    }
+
+#pragema mark Setters
+public:
+    /**
+     *  Sets the starting position of the player
+     *
+     *  @param startLoc The starting location of the player
+     */
+    void setPosition(Vec3 startLoc) {
+        _startLoc = startLoc;
+    }
+
+    /**
+     *  Sets the starting normal of the plane
+     *
+     *  @param startLoc The starting norm of the plane
+     */
+    void setPosition(Vec3 startNorm) {
+        _startNorm = startNorm;
+    }
+
+    /**
+     *  Sets the level mesh
+     *
+     *  @param mesh  The level mesh
+     */
+    void setMesh() {
+        return;
+    }
 
 
 #endif /* Level_h */
