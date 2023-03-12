4686d84fcb4b0196fc7bf0bd4edf16c3ad631426
Gordi Tenev <gst43@cornell.edu>
2023-03-12 16:49:41 -0400

    Revert "Delete MusicController.h"
    This reverts commit 8b7d17058976dcfa788348e28d6d6d233a601779.

---
+++ Pivot/source/MusicController.h
@@ -0,0 +1,22 @@
+//
+//  MusicController.h
+//  Platformer
+//
+//  Controls the music by providing functions that play music for different game modes
+//
+//  Created by Sarah Fleischman on 2/19/23.
+//
+
+#ifndef MusicController_h
+#define MusicController_h
+#include <cugl/cugl.h>
+#include "DataController.h"
+
+/**
+ *  Include functions that play music for different states
+ *
+ *  Include start music and stop music functions
+ */
+
+
+#endif /* MusicController_h */
