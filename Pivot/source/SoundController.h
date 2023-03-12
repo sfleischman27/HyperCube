598696ebb516183cb0ec06128e3e884313238869
Gordi Tenev <gst43@cornell.edu>
2023-03-12 16:53:23 -0400

    (need to rebuild) added soundcontroller + sound file
    run python cugl pivot

---
+++ Pivot/source/SoundController.h
@@ -0,0 +1,22 @@
+//
+//  SoundController.h
+//  Platformer
+//
+//  Controls the sound/music by providing functions that play music for different game modes
+//
+//  Created by Gordi Tenev on 3/12/23.
+//
+
+#ifndef SoundController_h
+#define SoundController_h
+#include <cugl/cugl.h>
+#include "DataController.h"
+
+/**
+ *  Include functions that play music for different states
+ *
+ *  Include start music and stop music functions
+ */
+
+
+#endif /* SoundController_h */
