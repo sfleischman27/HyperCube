096f24cdae588c5e0203eef55c175e3de5822628
Jolene Mei <xm87@cornell.edu>
2023-05-08 17:45:59 -0400

    change access

--- Pivot/source/Glowstick.h
+++ Pivot/source/Glowstick.h
@@ -21,8 +21,8 @@ public:
      * @param pos          The position of the glowstick
      */
     Glowstick(const Vec3 pos) : GameItem(pos) {
-        std::shared_ptr<cugl::Texture> tex =Texture::allocWithFile("spinner");
-        std::shared_ptr<cugl::Texture> norm =Texture::allocWithFile("spinner-normal");
+        std::shared_ptr<cugl::Texture> tex =Texture::allocWithFile("textures/barrier.png");
+        std::shared_ptr<cugl::Texture> norm =Texture::allocWithFile("textures/glowstick-normal.png");
         setTexture(tex);
         setNorm(norm);
         setColor(Vec3(0.0, 1.0, 0.2));
